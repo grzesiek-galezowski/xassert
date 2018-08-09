a0b206f53989515caa435038c225665d4dba86de
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2018-08-09 08:08:07 +0200

    Moved most of object graph assertions logic to GraphAssertions project

--- src/GraphAssertions/ObjectGraph.cs
+++ src/GraphAssertions/ObjectGraph.cs
@@ -2,6 +2,14 @@
 
 namespace GraphAssertions
 {
+  using System;
+  using System.Collections.Generic;
+  using System.Linq.Expressions;
+
+  using CommonTypes;
+
+  using TypeReflection;
+
   public static class ObjectGraph
   {
     public static CompareLogic Comparison()
@@ -25,7 +33,46 @@ namespace GraphAssertions
       return comparisonMechanism;
     }
 
+    public static ComparisonResult Compare<T>(T expected, T actual, string[] skippedPropertiesOrFields)
+    {
+      var comparison = ObjectGraph.Comparison();
+      foreach (var skippedMember in skippedPropertiesOrFields)
+      {
+        comparison.Config.MembersToIgnore.Add(skippedMember);
+      }
+      var result = comparison.Compare(expected, actual);
+      return result;
+    }
 
+    public static ComparisonResult Compare<T>(T expected, T actual, IEnumerable<Expression<Func<T, object>>> skippedPropertiesOrFields)
+    {
+      var comparison = ObjectGraph.Comparison();
+      foreach (var skippedPropertyOrField in skippedPropertiesOrFields)
+      {
+        var property = Property.FromUnaryExpression(skippedPropertyOrField);
+        var field = Field.FromUnaryExpression(skippedPropertyOrField);
+        if (property.HasValue)
+        {
+          Ignore(property, comparison);
+        }
+        else if (field.HasValue)
+        {
+          Ignore(field, comparison);
+        }
+      }
+      var result = comparison.Compare(expected, actual);
+      return result;
+    }
 
+    private static void Ignore(Maybe<Field> field, ICompareLogic comparison)
+    {
+      comparison.Config.MembersToIgnore.Add(field.Value.Name);
+    }
+
+    private static void Ignore(Maybe<Property> property, ICompareLogic comparison)
+    {
+      comparison.Config.MembersToIgnore.Add(property.Value.Name);
+      comparison.Config.MembersToIgnore.Add($"<{property.Value.Name}>k__BackingField");
+    }
   }
 }
--- src/TddToolkit/XAssert.ObjectGraph.cs
+++ src/TddToolkit/XAssert.ObjectGraph.cs
@@ -27,24 +27,25 @@ namespace TddEbook.TddToolkit
 
     public static void Alike<T>(T expected, T actual, params Expression<Func<T, object>>[] skippedPropertiesOrFields)
     {
-      var result = Compare(expected, actual, skippedPropertiesOrFields);
+      var result = ObjectGraph.Compare(expected, actual, skippedPropertiesOrFields);
       result.ExceededDifferences.Should().BeFalse(result.DifferencesString);
     }
 
     public static void NotAlike<T>(T expected, T actual, params Expression<Func<T, object>>[] skippedPropertiesOrFields)
     {
-      var result = Compare(expected, actual, skippedPropertiesOrFields);
+      var result = ObjectGraph.Compare(expected, actual, skippedPropertiesOrFields);
       result.ExceededDifferences.Should().BeTrue(result.DifferencesString);
     }
 
     public static void Alike<T>(T expected, T actual, params string[] skippedPropertiesOrFields)
     {
-      var result = Compare(expected, actual, skippedPropertiesOrFields);
+      var result = ObjectGraph.Compare(expected, actual, skippedPropertiesOrFields);
       result.ExceededDifferences.Should().BeFalse(result.DifferencesString);
     }
+
     public static void NotAlike<T>(T expected, T actual, params string[] skippedPropertiesOrFields)
     {
-      var result = Compare(expected, actual, skippedPropertiesOrFields);
+      var result = ObjectGraph.Compare(expected, actual, skippedPropertiesOrFields);
       result.ExceededDifferences.Should().BeTrue(result.DifferencesString);
     }
 
@@ -55,52 +56,12 @@ namespace TddEbook.TddToolkit
     }
 
     //todo move to separate class in ObjectGraph assembly
-    private static ComparisonResult Compare<T>(T expected, T actual, string[] skippedPropertiesOrFields)
-    {
-      var comparison = ObjectGraph.Comparison();
-      foreach (var skippedMember in skippedPropertiesOrFields)
-      {
-        comparison.Config.MembersToIgnore.Add(skippedMember);
-      }
-      var result = comparison.Compare(expected, actual);
-      return result;
-    }
 
     // todo same as above
-    private static ComparisonResult Compare<T>(T expected, T actual, IEnumerable<Expression<Func<T, object>>> skippedPropertiesOrFields)
-    {
-      var comparison = ObjectGraph.Comparison();
-      foreach (var skippedPropertyOrField in skippedPropertiesOrFields)
-      {
-        var property = Property.FromUnaryExpression(skippedPropertyOrField);
-        var field = Field.FromUnaryExpression(skippedPropertyOrField);
-        if (property.HasValue)
-        {
-          Ignore(property, comparison);
-        }
-        else if (field.HasValue)
-        {
-          Ignore(field, comparison);
-        }
-      }
-      var result = comparison.Compare(expected, actual);
-      return result;
-    }
 
     // todo same as above
-    private static void Ignore(Maybe<Field> field, ICompareLogic comparison)
-    {
-      comparison.Config.MembersToIgnore.Add(field.Value.Name);
-    }
 
     // todo same as above
-    private static void Ignore(Maybe<Property> property, ICompareLogic comparison)
-    {
-      comparison.Config.MembersToIgnore.Add(property.Value.Name);
-      comparison.Config.MembersToIgnore.Add($"<{property.Value.Name}>k__BackingField");
-    }
-
-
   }
 
   //bug this is unfinished and does not work yet!!
