4fff1b4f384fb6efcffc9c7c7bd42bdba3f158a9
Galezowski Grzegorz <grzesiek.galezowski@gmail.com>
2018-08-17 09:03:54 +0200

    Testing the versioning plugin

--- src/old/TddToolkitSpecification/Properties/AssemblyInfo.cs
+++ src/old/TddToolkitSpecification/Properties/AssemblyInfo.cs
@@ -13,12 +13,12 @@ using System.Reflection;
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
 
-[assembly: AssemblyInformationalVersion("0.1.0+47.Branch.master.Sha.057f02d6bac7a3486a5c0fae0112db3049772a73")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.1+Branch.master.Sha.a87d591fb707663fa92631140a58a17a8578aa04")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
